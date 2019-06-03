8eb8a13adfd94fb0378481c27812996926247edc
Jessica Cherayil <jessicacherayil@Jessicas-MacBook-Air.local>
2019-06-03 16:51:35 -0500

    cleaning up form

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  $("#contactform input,#contactform textarea").jqBootstrapValidation({
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
       // additional error messages or events
